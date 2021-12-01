fb4ba3d63de8b96856f47379d30e0737b33a7d1f
Paul <bl0cksb3rg@gmail.com>
2021-12-01 11:28:59 +0100

    design

--- js/script_authentication.js
+++ js/script_authentication.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     $(".veen .rgstr-btn button").click(function() {
         $('.veen .wrapper').addClass('move');
-        $('.body').css('background', '#e0b722');
+        $('.body').css('background', '#3b8a88');
         $(".veen .login-btn button").removeClass('active');
         $(this).addClass('active');
 
     });
     $(".veen .login-btn button").click(function() {
         $('.veen .wrapper').removeClass('move');
-        $('.body').css('background', '#ff4931');
+        $('.body').css('background', '#0b2b4d');
         $(".veen .rgstr-btn button").removeClass('active');
         $(this).addClass('active');
     });
