f2e7074bee681310d781592002901bb3782288bf
seemannmodnar <miko0401@hotmail.com>
2022-01-28 08:55:17 +0000

    some changes

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo docker run -d -it -p 443:443 -p 80:80 -v $(pwd)/routes:/usr/share/nginx/html --rm --name terminator-jq terminator-app-jq
+sudo docker run -d -it -p 80:80 -v $(pwd):/usr/share/nginx/html --rm --name terminator-jq terminator-app-jq
 
 # /bin/ash -c "npm install --global http-server && http-server";
