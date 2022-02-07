14465c66feb137c83a25596628c77af4f799d29f
Christian Sander <christian.sander@awi.de>
2022-02-07 16:31:31 +0100

    Das muss kesseln!

--- dataTables.php
+++ dataTables.php
@@ -16,6 +16,57 @@
                 ];
             }
             break;
+        case "pastReservations":
+            foreach (range(1, rand(2, 10)) as $lol) {
+                $data[] = [
+                    "id" => rand(1,10),
+                    "time" => date_format(date_create(), "d.m.Y, H:i:s"),
+                    "description" => "Beim Jupiter!",
+                    "room" => 1337,
+                    "booker" => "Hellmut Dunkelangst"
+                ];
+            }
+            break;
+        case "buildings":
+            $buildings = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+            foreach (range(1, rand(2, 10)) as $lol) {
+                $data[] = [
+                    "id" => $lol,
+                    "name" => $buildings[array_rand($buildings)],
+                    "description" => "Geisterhaus" . $lol,
+                ];
+            }
+            break;
+        case "rooms":
+            $buildings = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+            foreach (range(1, rand(2, 10)) as $lol) {
+                $data[] = [
+                    "id" => $lol,
+                    "name" => array_rand($buildings),
+                    "description" => "Geisterzimmer" . $lol,
+                    "building" => $buildings[array_rand($buildings)]
+                ];
+            }
+            break;
+        case "roles":
+            $names = ["Ali Fikdusimir", "Peter Goge", "Achmed Ach Lachnet"];
+            $groups = [1, 2, 3];
+            foreach (range(1, rand(2, 10)) as $lol) {
+                if (array_key_exists("category", $_GET) && in_array($_GET["category"], $groups)) {
+                    $data[] = [
+                        "id" => $lol,
+                        "name" => $names[array_rand($names)]
+                    ];
+                }
+                else {
+                    $data[] = [
+                        "id" => $lol,
+                        "name" => $names[array_rand($names)],
+                        "group" => $groups[array_rand($groups)]
+                    ];
+                }
+            }
+            break;
         default:
             $data = null;
             break;
