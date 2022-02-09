9497cb9c3de273f504a4c7b8a4a8e89a8f54cffa
Christian Sander <christian.sander@awi.de>
2022-02-09 16:30:30 +0100

    Heiliges Kanonenrohr!

---
+++ calendar.php
@@ -0,0 +1,9 @@
+<?php
+    echo json_encode([
+        ["start" => "2022-02-10", "title" => "lol", "id" => 1],
+        ["start" => "2022-02-15T10:00:00", "end" => "2022-02-15T11:00:00", "title" => "lol", "id" => 2, "extendedProps" => ["booker" => "Hellmut Dunkelangst"]],
+        ["start" => "2022-02-16T10:00:00", "end" => "2022-02-16T11:00:00", "title" => "lol", "id" => 3, "extendedProps" => ["booker" => "Hellmut Dunkelangst"]],
+        ["start" => "2022-02-08T10:00:00", "end" => "2022-02-08T11:00:00", "title" => "looooooooooooooooool", "id" => 4, "extendedProps" => ["booker" => "Hellmut Dunkelangst"]]
+    ]);
+    return;
+?>
