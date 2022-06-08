e1f0aa569a1193f8abcb60f2688ac6609e54f366
CHRISTIAN\Sonic <aso@aso.wtf>
2022-06-08 21:32:01 +0200

    Ich hol' gleich aus, du!

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM nginx:latest
-
-ADD ./conf/default.conf /etc/nginx/conf.d/default.conf
-#ADD ./routes /usr/share/nginx/html
-
-RUN echo "start nginx"
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo docker run -d -it -p 80:80 -v $(pwd):/usr/share/nginx/html --rm --name terminator-jq terminator-app-jq
-
-# /bin/ash -c "npm install --global http-server && http-server";
